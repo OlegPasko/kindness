f8d66c1eb7efb96bd3ca5fcb3beb744dcee154fa
Oleg Pasko <olegpasko@gmail.com>
2012-12-26 13:49:40 +0200

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kindness::Application.config.secret_token = '6fc759753d5aeb7cc0ee4bed5d2f59413e64d6c7348d7b91bed543964b6a1f72e6bb21d8a9923d5444872b89e79f693e4e56850757d20ca3a12827309c27708f'
